c6fb6d5efeff4c349daba306685a7ad20aa3401a
Eol <eol@eolstudy.com>
2021-02-18 17:15:32 +0800

    keypair setup worked

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import { createModule, extractVuexModule } from 'vuex-class-component'
+import { action, createModule, createProxy, extractVuexModule } from 'vuex-class-component'
+import * as openpgp from 'openpgp'
+
+interface ECCConfig {
+  algorithm: 'ECC'
+  option: 'curve25519' | 'ed25519' | 'p256' | 'p384' | 'p521' | 'secp256k1' | 'brainpoolP256r1' | 'brainpoolP384r1' | 'brainpoolP512r1'
+}
+
+interface RSAConfig {
+  algorithm: 'RSA'
+  option: 2048 | 4096
+}
+
+type KeyConfig = (ECCConfig | RSAConfig) & {passphrase: string}
 
 Vue.use(Vuex)
 
@@ -9,12 +22,31 @@ const vuexModule = createModule({
 })
 
 class Store extends vuexModule {
-  private testString = 'private string'
-  get test_string () {
-    return this.testString
+  public localPubKey = ''
+  public localPriKey = ''
+  public remotePubKey = ''
+  private _keyConfig: KeyConfig = {
+    algorithm: 'ECC',
+    option: 'ed25519',
+    passphrase: ''
+  }
+
+  @action public async gen_keypair () {
+    const newKeyPair = await openpgp.generateKey({
+      userIds: [{}],
+      passphrase: this._keyConfig.passphrase,
+      curve: this._keyConfig.algorithm === 'ECC' ? this._keyConfig.option : undefined,
+      numBits: this._keyConfig.algorithm === 'RSA' ? this._keyConfig.option : undefined
+    })
+    this.localPubKey = newKeyPair.publicKeyArmored
+    this.localPriKey = newKeyPair.privateKeyArmored
   }
 }
 
-export default new Vuex.Store({
+const store = new Vuex.Store({
   ...extractVuexModule(Store).store
 })
+
+export const storeProxy = createProxy(store, Store)
+export default store
+export type { KeyConfig }
--- src/views/PPGP.vue
+++ src/views/PPGP.vue
@@ -2,23 +2,40 @@
   div
     p Generate new keypair or import from local.
     p
-      button(@click="gen_keypair") New
-      button(@click="import_keypair" disabled) Import
+      button(@click="store.gen_keypair") New
+      //- button(@click="import_keypair" disabled) Import
     p
       | Public Key
-      textarea(v-module="pubKey" disabled)
+      textarea(v-model="store.localPubKey" disabled)
     p
       | Private Key
-      textarea(v-module="priKey" disabled)
+      textarea(v-model="store.localPriKey" disabled)
+    p Input remote public key bellow
+    p
+      textarea(v-model="store.remotePubKey")
+    p
+      button(
+        :disabled="!setupReady"
+        @click=""
+      ) Confirm
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import Component from 'vue-class-component'
 
+import { storeProxy } from '@/store'
+
 @Component
 export default class PPGP extends Vue {
-  private pubKey!: string
-  private priKey!: string
+  store = storeProxy
+
+  get setupReady (): boolean {
+    return !!storeProxy.localPubKey.length && !!storeProxy.localPriKey.length && !!storeProxy.remotePubKey.length
+  }
+
+  // private import_keypair (): void {
+
+  // }
 }
 </script>
